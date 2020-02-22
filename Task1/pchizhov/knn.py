4e1fee8d377e72ea7ed9e8bc523ab1fd9ac81880
pchizhov <chizhov.pasha@yandex.ru>
2020-02-22 13:15:41 +0300

    Task 1.1 done

--- Task1/pchizhov/knn.py
+++ Task1/pchizhov/knn.py
@@ -54,8 +54,8 @@ class KNN:
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
             for i_train in range(num_train):
-                # TODO: Fill dists[i_test][i_train]
-                pass
+                dists[i_test][i_train] = np.sum(np.abs(self.train_X[i_train] - X[i_test]))
+        return dists
 
     def compute_distances_one_loop(self, X):
         '''
@@ -73,9 +73,8 @@ class KNN:
         num_test = X.shape[0]
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
-            # TODO: Fill the whole row of dists[i_test]
-            # without additional loops or list comprehensions
-            pass
+            dists[i_test] = np.sum(np.abs(self.train_X - X[i_test]), axis=1)
+        return dists
 
     def compute_distances_no_loops(self, X):
         '''
@@ -93,8 +92,8 @@ class KNN:
         num_test = X.shape[0]
         # Using float32 to to save memory - the default is float64
         dists = np.zeros((num_test, num_train), np.float32)
-        # TODO: Implement computing all distances with no loops!
-        pass
+        dists = np.sum(np.abs(self.train_X[None, :] - X[:, None]), axis=2)
+        return dists
 
     def predict_labels_binary(self, dists):
         '''
@@ -111,9 +110,8 @@ class KNN:
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.bool)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
+            pred[i] = np.argmax(np.bincount(self.train_y[np.argsort(dists[i])[:self.k]]))
             # nearest training samples
-            pass
         return pred
 
     def predict_labels_multiclass(self, dists):
@@ -129,10 +127,8 @@ class KNN:
            for every test sample
         '''
         num_test = dists.shape[0]
-        num_test = dists.shape[0]
         pred = np.zeros(num_test, np.int)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
+            pred[i] = np.argmax(np.bincount(self.train_y[np.argsort(dists[i])[:self.k]]))
             # nearest training samples
-            pass
         return pred
--- Task1/pchizhov/metrics.py
+++ Task1/pchizhov/metrics.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def binary_classification_metrics(prediction, ground_truth):
     '''
     Computes metrics for binary classification
@@ -9,16 +12,13 @@ def binary_classification_metrics(prediction, ground_truth):
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
+    true_positives = np.sum((prediction == 1) & (ground_truth == 1))
 
-    # TODO: implement metrics!
-    # Some helpful links:
-    # https://en.wikipedia.org/wiki/Precision_and_recall
-    # https://en.wikipedia.org/wiki/F1_score
-    
+    precision = true_positives / np.sum(prediction == 1)
+    recall = true_positives / np.sum(ground_truth == 1)
+    f1 = 2 * precision * recall / (precision + recall)
+    accuracy = np.sum(prediction == ground_truth) / prediction.shape[0]
+
     return precision, recall, f1, accuracy
 
 
@@ -33,5 +33,5 @@ def multiclass_accuracy(prediction, ground_truth):
     Returns:
     accuracy - ratio of accurate predictions to total samples
     '''
-    # TODO: Implement computing accuracy
-    return 0
+    accuracy = np.sum(prediction == ground_truth) / prediction.shape[0]
+    return accuracy
