1d5e1a0b9b3f7b72152e829f31bc36d541455e7a
01gaM <olga.molchanova.olkamana@gmail.com>
2020-02-23 23:53:37 +0300

    Task 2.1
    Neural net test set accuracy: 0.182000

--- Task2/01gaM/gradient_check.py
+++ Task2/01gaM/gradient_check.py
@@ -29,8 +29,10 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
         analytic_grad_at_ix = analytic_grad[ix]
         numeric_grad_at_ix = 0
 
-        # TODO Copy from previous assignment
-        raise Exception("Not implemented!")
+        # DONE: Copy from previous assignment
+        delta_v = np.zeros(x.shape)
+        delta_v[ix] = delta
+        numeric_grad_at_ix = (f(x + delta_v)[0] - f(x - delta_v)[0]) / (2*delta) 
 
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
