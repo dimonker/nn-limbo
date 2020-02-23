5b194ac9a704f5c3de60281cfd37b3a6f6dabb8d
RenataKharisova <renata003@mail.ru>
2020-02-23 13:46:05 +0300

    Task 1.1

--- Task1/Re1199/metrics.py
+++ Task1/Re1199/metrics.py
@@ -9,16 +9,45 @@ def binary_classification_metrics(prediction, ground_truth):
     Returns:
     precision, recall, f1, accuracy - classification metrics
     '''
-    precision = 0
-    recall = 0
-    accuracy = 0
-    f1 = 0
-
     # TODO: implement metrics!
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
-    
+
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    len_predict = len(prediction)
+
+    for i in range(len_predict):
+        if prediction[i] == ground_truth[i]:
+            if prediction[i]:
+                tp += 1
+            else:
+                tn += 1
+        elif prediction[i]:
+            fp += 1
+        else:
+            fn += 1
+
+    if (tp + fp) != 0:
+        precision = tp / (tp + fp)
+    else:
+        precision = 0
+
+    if (tp + fn) != 0:
+        recall = tp / (tp + fn)
+    else:
+        recall = 0
+
+    accuracy = (tp + tn) / len_predict
+
+    if (precision + recall) != 0:
+        f1 = (2 * recall * precision) / (precision + recall)
+    else:
+        f1 = 0
+
     return precision, recall, f1, accuracy
 
 
@@ -34,4 +63,13 @@ def multiclass_accuracy(prediction, ground_truth):
     accuracy - ratio of accurate predictions to total samples
     '''
     # TODO: Implement computing accuracy
-    return 0
+
+    accuracy = 0
+    len_predict = len(prediction)
+
+    for i in range(len_predict):
+        if prediction[i] == ground_truth[i]:
+            accuracy += 1
+
+    return accuracy / len_predict
+
