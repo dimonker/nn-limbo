df5052fd974b6938fd3e0ceabd5d8074b155dcad
dmitry <rebrik.dmitry@gmail.com>
2020-02-24 00:05:01 +0300

    task 2.1  done

--- Task2/dimonker/model.py
+++ Task2/dimonker/model.py
@@ -9,7 +9,6 @@ class TwoLayerNet:
     def __init__(self, n_input, n_output, hidden_layer_size, reg):
         """
         Initializes the neural network
-
         Arguments:
         n_input, int - dimension of the model input
         n_output, int - number of classes to predict
@@ -17,56 +16,53 @@ class TwoLayerNet:
         reg, float - L2 regularization strength
         """
         self.reg = reg
-        # TODO Create necessary layers
-        raise Exception("Not implemented!")
+        self.fc1 = FullyConnectedLayer(n_input, hidden_layer_size)
+        self.ReLU1 = ReLULayer()
+        self.fc2 = FullyConnectedLayer(hidden_layer_size, n_output)
 
     def compute_loss_and_gradients(self, X, y):
         """
         Computes total loss and updates parameter gradients
         on a batch of training examples
-
         Arguments:
         X, np array (batch_size, input_features) - input data
         y, np array of int (batch_size) - classes
         """
-        # Before running forward and backward pass through the model,
-        # clear parameter gradients aggregated from the previous pass
-        # TODO Set parameter gradient to zeros
-        # Hint: using self.params() might be useful!
-        raise Exception("Not implemented!")
-        
-        # TODO Compute loss and fill param gradients
-        # by running forward and backward passes through the model
-        
-        # After that, implement l2 regularization on all params
-        # Hint: self.params() is useful again!
-        raise Exception("Not implemented!")
+        params = self.params()
+        for key in params.keys():
+            params[key].grad = np.zeros_like(params[key].value)
+
+            out1 = self.ReLU1.forward(self.fc1.forward(X))
+        out2 = self.fc2.forward(out1)
+        loss, grad = softmax_with_cross_entropy(out2, y)
+        for key in params.keys():
+            l2_loss, l2_grad = l2_regularization(params[key].value, self.reg)
+            loss += l2_loss
+            params[key].grad += l2_grad
+        d_out2 = self.fc2.backward(grad)
+        self.fc1.backward(self.ReLU1.backward(d_out2))
 
         return loss
 
     def predict(self, X):
         """
         Produces classifier predictions on the set
-
         Arguments:
           X, np array (test_samples, num_features)
-
         Returns:
           y_pred, np.array of int (test_samples)
         """
-        # TODO: Implement predict
-        # Hint: some of the code of the compute_loss_and_gradients
-        # can be reused
-        pred = np.zeros(X.shape[0], np.int)
-
-        raise Exception("Not implemented!")
+        out1 = self.ReLU1.forward(self.fc1.forward(X))
+        out2 = self.fc2.forward(out1)
+        pred = out2.argmax(axis=1)
         return pred
 
     def params(self):
         result = {}
 
-        # TODO Implement aggregating all of the params
-
-        raise Exception("Not implemented!")
+        result['W1'] = self.fc1.W
+        result['B1'] = self.fc1.B
+        result['W2'] = self.fc2.W
+        result['B2'] = self.fc2.B
 
         return result
