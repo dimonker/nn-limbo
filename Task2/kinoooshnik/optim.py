c7b2267bacbb4e5e9431614af363b584413ea186
Alexander Semiletov <sasha.semiletow2011@yandex.ru>
2020-02-26 20:20:43 +0000

    Task 2.1

--- Task2/kinoooshnik/optim.py
+++ Task2/kinoooshnik/optim.py
@@ -26,6 +26,7 @@ class MomentumSGD:
     """
     def __init__(self, momentum=0.9):
         self.momentum = 0.9
+        self.velocity = None
     
     def update(self, w, d_w, learning_rate):
         """
@@ -42,4 +43,7 @@ class MomentumSGD:
         # TODO Implement momentum update
         # Hint: you'll need to introduce some variables to remember
         # velocity from the previous updates
-        raise Exception("Not implemented!")        
+        if type(self.velocity) == type(None):
+            self.velocity = np.zeros_like(d_w)
+        self.velocity = self.momentum * self.velocity - learning_rate * d_w
+        return w + self.velocity
