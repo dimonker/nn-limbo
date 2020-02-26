93086d33fd4b740ed204111e66f0828d898329cc
4ound <lnfedorov18@gmail.com>
2020-02-26 23:57:00 +0300

    task 2.1

--- Task2/4ound/model.py
+++ Task2/4ound/model.py
@@ -17,8 +17,9 @@ class TwoLayerNet:
         reg, float - L2 regularization strength
         """
         self.reg = reg
-        # TODO Create necessary layers
-        raise Exception("Not implemented!")
+        self.fc1 = FullyConnectedLayer(n_input, hidden_layer_size)
+        self.ReLU1 = ReLULayer()
+        self.fc2 = FullyConnectedLayer(hidden_layer_size, n_output)
 
     def compute_loss_and_gradients(self, X, y):
         """
@@ -29,18 +30,21 @@ class TwoLayerNet:
         X, np array (batch_size, input_features) - input data
         y, np array of int (batch_size) - classes
         """
-        # Before running forward and backward pass through the model,
-        # clear parameter gradients aggregated from the previous pass
-        # TODO Set parameter gradient to zeros
-        # Hint: using self.params() might be useful!
-        raise Exception("Not implemented!")
-        
-        # TODO Compute loss and fill param gradients
-        # by running forward and backward passes through the model
-        
-        # After that, implement l2 regularization on all params
-        # Hint: self.params() is useful again!
-        raise Exception("Not implemented!")
+        for param_ix in self.params():
+            self.params()[param_ix] = np.zeros_like(self.params()[param_ix].value)
+
+        out1 = self.ReLU1.forward(self.fc1.forward(X))
+        out2 = self.fc2.forward(out1)
+
+        loss, grad = softmax_with_cross_entropy(out2, y)
+
+        d_out2 = self.fc2.backward(grad)
+        self.fc1.backward(self.ReLU1.backward(d_out2))
+
+        for param_ix in self.params():
+            l2_loss, l2_grad = l2_regularization(self.params()[param_ix].value, self.reg)
+            loss += l2_loss
+            self.params()[param_ix].grad += l2_grad
 
         return loss
 
@@ -54,19 +58,9 @@ class TwoLayerNet:
         Returns:
           y_pred, np.array of int (test_samples)
         """
-        # TODO: Implement predict
-        # Hint: some of the code of the compute_loss_and_gradients
-        # can be reused
-        pred = np.zeros(X.shape[0], np.int)
-
-        raise Exception("Not implemented!")
-        return pred
+        out1 = self.ReLU1.forward(self.fc1.forward(X))
+        out2 = self.fc2.forward(out1)
+        return out2.argmax(axis=1)
 
     def params(self):
-        result = {}
-
-        # TODO Implement aggregating all of the params
-
-        raise Exception("Not implemented!")
-
-        return result
+        return {'W1': self.fc1.W, 'B1': self.fc1.B, 'W2': self.fc2.W, 'B2': self.fc2.B}
