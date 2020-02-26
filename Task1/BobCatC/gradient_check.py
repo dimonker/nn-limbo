ead0a4607ea47be446ab28150afdefdbf3814e5c
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2020-02-26 23:03:57 +0300

    Completed Task 1.2 (Linear classification)

--- Task1/BobCatC/gradient_check.py
+++ Task1/BobCatC/gradient_check.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def check_gradient(f, x, delta=1e-5, tol = 1e-4):
+def check_gradient(f, x, delta=1e-5, tol=1e-4):
     '''
     Checks the implementation of analytical gradient by comparing
     it to numerical gradient using two-point formula
@@ -15,10 +15,10 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     Return:
       bool indicating whether gradients match or not
     '''
-    
+
     assert isinstance(x, np.ndarray)
     assert x.dtype == np.float
-    
+
     orig_x = x.copy()
     fx, analytic_grad = f(x)
     assert np.all(np.isclose(orig_x, x, tol)), "Functions shouldn't modify input variables"
@@ -32,18 +32,22 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        numeric_grad_at_ix = 0
 
-        # TODO compute value of numeric gradient of f to idx
+        x1 = x.copy()
+        x1[ix] += delta
+        x2 = x.copy()
+        x2[ix] -= delta
+        f1, _ = f(x1)
+        f2, _ = f(x2)
+        numeric_grad_at_ix = (f1 - f2) / (2 * delta)
+
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
-            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
+            print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (
+                ix, analytic_grad_at_ix, numeric_grad_at_ix)
+            )
             return False
 
         it.iternext()
 
     print("Gradient check passed!")
     return True
-
-        
-
-        
